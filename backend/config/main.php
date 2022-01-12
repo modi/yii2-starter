17eed3583c1014ce43e7e2d080a5bf2898cd6b65
Mo Di <mdqebb@gmail.com>
2022-01-12 12:40:00 +0800

    启用 URL 静态化

--- backend/config/main.php
+++ backend/config/main.php
@@ -32,14 +32,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -31,14 +31,12 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
