af7be8029559a1c32d94ecd25bb386806e7b5ba1
Mo Di <mdqebb@gmail.com>
2022-03-07 11:43:46 +0800

    修正 test 环境 db 配置重复的问题

--- common/config/test.php
+++ common/config/test.php
@@ -5,11 +5,6 @@ return [
     'basePath' => dirname(__DIR__),
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
-        ],
-    ],
-    'components' => [
-        'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => $_ENV['DB_TEST_DSN'],
             'username' => $_ENV['DB_TEST_USERNAME'],
